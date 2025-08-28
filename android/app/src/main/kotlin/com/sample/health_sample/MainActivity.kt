3a011e5b97c50489dd6a0ebc945c139ac0c412b5
Tasuku Maeda <maeda.tasuku@classmethod.jp>
2025-08-28 15:55:01 +0900

    AndroidのHealth関連の設定追加

--- android/app/src/main/kotlin/com/sample/health_sample/MainActivity.kt
+++ android/app/src/main/kotlin/com/sample/health_sample/MainActivity.kt
@@ -3,11 +3,12 @@ package com.sample.health_sample
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import io.flutter.embedding.android.FlutterActivity
+//import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
     private val TAG = "MainActivity"
     private val CHANNEL = "com.example.health_sample"
 
