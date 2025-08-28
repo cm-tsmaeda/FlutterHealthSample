f8515488b6644cee53addff36b1bb1076948a29c
Tasuku Maeda <maeda.tasuku@classmethod.jp>
2025-08-28 16:06:26 +0900

    iOS側のMethodChannel追加

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,23 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      
+      let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+      let nativeChannel = FlutterMethodChannel(name: "com.example.health_sample",
+                                             binaryMessenger: controller.binaryMessenger)
+      nativeChannel.setMethodCallHandler({
+        (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        
+        switch call.method {
+        case "openPrivacyPolicy":
+          print("openPrivacyPolicy called")
+        default:
+            print("not implemented")
+          result(FlutterMethodNotImplemented)
+        }
+      })
+      
+      
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
